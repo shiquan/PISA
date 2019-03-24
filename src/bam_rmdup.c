ba7a6b4f366f8bb379447fef5c992128824d0cbd
Shi Quan <shiquan@link.cuhk.edu.hk>
2019-03-24 18:26:11 +0800

    debugging

--- src/bam_rmdup.c
+++ src/bam_rmdup.c
@@ -121,6 +121,8 @@ static void memory_release()
     hts_close(args.fp);
     bgzf_close(args.out);
     bam_hdr_destroy(args.hdr);
+    for (i = 0; i < args.n_tag; ++i) free(args.tags[i]);
+    free(args.tags);
 }
 // todo: improve allocated and free SAM pool
 struct sam_pool {
