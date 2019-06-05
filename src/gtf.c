784e36f04b53dd8e5c0af066e90e171b1e78dd73
Shi Quan <shiquan@link.cuhk.edu.hk>
2019-06-05 21:17:15 +0800

    fixed bugs annotate gene

--- src/gtf.c
+++ src/gtf.c
@@ -326,7 +326,7 @@ struct gtf_lite *gtf_overlap_gene(struct gtf_spec *G, char *name, int start, int
     // find the smallest i such that idx_end >= st
     while (st < ed) {
         int mid = st + ((end-start)>>1);
-        if (idx_end(G->idx[mid])>=start) st = mid;
+        if (idx_end(G->idx[mid])<start) st = mid;
         else ed = mid;
     }
     assert(st == ed);
