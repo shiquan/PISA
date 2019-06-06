fd3f2af0047a52197b4b004a34f64ab954bf7929
Shi Quan <shiquan@link.cuhk.edu.hk>
2019-06-06 21:26:32 +0800

    fix memory bug

--- src/bam_anno.c
+++ src/bam_anno.c
@@ -377,7 +377,7 @@ void memory_release()
     bam_hdr_destroy(args.hdr);
     sam_close(args.fp);
     sam_close(args.out);
-    if (args.B) destroy_bed(args.B);
+    if (args.B) destory_bed(args.B);
     else gtf_destory(args.G);
     if (args.fp_report != stderr) fclose(args.fp_report);
 }
