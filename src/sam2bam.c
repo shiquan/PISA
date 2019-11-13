8beb0b8e702798496a739738b7ace3f3878cbbc8
Shi Quan <shiquan@link.cuhk.edu.hk>
2019-11-13 09:39:35 +0800

    modified:   src/sam2bam.c

--- src/sam2bam.c
+++ src/sam2bam.c
@@ -284,6 +284,7 @@ static void summary_report(struct args *opts)
         fprintf(opts->fp_report,"Mapping quals above %d,%"PRIu64"\n", opts->qual_thres, summary->n_qual);
         fprintf(opts->fp_report,"Mitochondria ratio,%.2f%%\n", (float)summary->n_mito/summary->n_mapped*100);
         fprintf(opts->fp_report,"Usable reads (ratio),%"PRIu64" (%.2f%%)\n", summary->n_usable, (float)summary->n_usable/summary->n_reads*100);
+        fprintf(opts->fp_report,"Failed to parse reads,%"PRIu64"\n", summary->n_failed_to_parse);
     }
 
     free(summary);
