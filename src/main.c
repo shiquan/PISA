029d77da61a3a17aa16d34a84fe5d5ef6728418b
Shi Quan <shiquan@link.cuhk.edu.hk>
2020-04-04 21:30:39 +0200

    update usage

--- src/main.c
+++ src/main.c
@@ -12,22 +12,17 @@ int usage()
     fprintf(stderr, "\n--- Processing FASTQ\n");
     fprintf(stderr, "    parse      Parse barcodes from fastq reads.\n");
     fprintf(stderr, "    fsort      Sort fastq records by barcodes.\n");
-    //fprintf(stderr, "    assem      Assemble tagged reads. Fastq need be sorted by `fsort` before assembly ** experiment **.\n");
-    //fprintf(stderr, "    segment    Select defined segments from contigs. ** experiment **\n");
     
     fprintf(stderr, "\n--- Processing BAM\n");
     fprintf(stderr, "    sam2bam    Convert SAM to BAM, and transform tags from read name to standard SAM tags.\n");
-    fprintf(stderr, "    rmdup      Remove PCR duplicates, consider cell barcodes and/or UMIs.\n");
-    fprintf(stderr, "    pick       Pick alignments of specific cells.\n");
+    fprintf(stderr, "    rmdup      Remove PCR duplicates per cell barcodes and/or UMIs.\n");
+    fprintf(stderr, "    pick       Pick alignments with tags.\n");
     fprintf(stderr, "    anno       Annotate functional regions or gene names.\n");
-    fprintf(stderr, "    corr       Correct error prone UMIs. 1 mismatch considered.\n");
-    fprintf(stderr, "    attrcnt    Count raw reads and tags.\n");
-    fprintf(stderr, "    extract    Extract tags information from BAM.\n");
+    fprintf(stderr, "    corr       Correct error prone UMIs. 1 mismatch considered in default.\n");
+    fprintf(stderr, "    attrcnt    Count raw reads and tag values per cell.\n");
+    fprintf(stderr, "    extract    Extract tag value from BAM.\n");
     fprintf(stderr, "    count      Count matrix.\n");
-    fprintf(stderr, "    bam2fq     Convert bam to fastq file. Tagged information will exported at read name.\n");
-    // fprintf(stderr, "    genecov    Calculate reads coverage over gene body.\n");
-    // fprintf(stderr, "    bam2frag   Convert bam to bgzipped fragment file.\n");
-    // fprintf(stderr, "    impute     Impute tagged reads.\n");
+    fprintf(stderr, "    bam2fq     Convert BAM to FASTQ+ file with selected tags.\n");
     
     fprintf(stderr, "\n");
     return 1;
@@ -48,17 +43,9 @@ int main(int argc, char *argv[])
     extern int bam_extract_tags(int argc, char **argv);
     extern int count_matrix(int argc, char *argv[]);
     extern int bam2fq(int argc, char *argv[]);
-    extern int gene_cov(int argc, char **argv);
-    extern int bam2frag(int argc, char **argv);
-    // process scLFR
-    //extern int assem(int argc, char **argv);
-    //extern int check_segment(int argc, char **argv);
-    //extern int check_segment2(int argc, char **argv);
-    extern int fastq_segment(int argc, char **argv);
-    //extern int LFR_cleanup(int argc, char **argv);
-    extern int fastq_assem(int argc, char **argv);
-    //extern int fastq_overlap(int argc, char **argv);
-    extern int LFR_impute(int argc, char **argv);
+    // extern int gene_cov(int argc, char **argv);
+    // extern int bam2frag(int argc, char **argv);
+
 
     if (argc == 1) return usage();
     else if (strcmp(argv[1], "parse") == 0) return fastq_prase_barcodes(argc-1, argv+1);
@@ -72,15 +59,15 @@ int main(int argc, char *argv[])
     else if (strcmp(argv[1], "attrcnt") == 0) return bam_count_attr(argc-1, argv+1);
     else if (strcmp(argv[1], "extract") == 0) return bam_extract_tags(argc-1, argv+1);
     else if (strcmp(argv[1], "pick") == 0) return bam_pick(argc-1, argv+1);
-    else if (strcmp(argv[1], "genecov") == 0) return gene_cov(argc-1, argv+1);
-    else if (strcmp(argv[1], "bam2frag") == 0) return bam2frag(argc-1, argv+1);
+    // else if (strcmp(argv[1], "genecov") == 0) return gene_cov(argc-1, argv+1);
+    // else if (strcmp(argv[1], "bam2frag") == 0) return bam2frag(argc-1, argv+1);
     else if (strcmp(argv[1], "count") == 0) return count_matrix(argc-1, argv+1);
-    else if (strcmp(argv[1], "assem") == 0)  return fastq_assem(argc-1, argv+1);
-    else if (strcmp(argv[1], "segment") == 0) return fastq_segment(argc-1, argv+1);
+    // else if (strcmp(argv[1], "assem") == 0)  return fastq_assem(argc-1, argv+1);
+    // else if (strcmp(argv[1], "segment") == 0) return fastq_segment(argc-1, argv+1);
     // else if (strcmp(argv[1], "segment2") == 0) return check_segment2(argc-1, argv+1);
     //else if (strcmp(argv[1], "cleanup") == 0) return LFR_cleanup(argc-1, argv+1);
     //else if (strcmp(argv[1], "overlap") == 0) return fastq_overlap(argc-1, argv+1);
-    else if (strcmp(argv[1], "impute") == 0) return LFR_impute(argc-1, argv+1);                    
+    // else if (strcmp(argv[1], "impute") == 0) return LFR_impute(argc-1, argv+1);                    
     else return usage();
     return 0;
 }
